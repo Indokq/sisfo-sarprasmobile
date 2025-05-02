dda8916523bfe21d068666785436948d10b3ed51
Indokq <arsylfauzan19@gmail.com>
2025-05-02 21:27:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart'; // Pastikan file ini berada di direktori yang benar
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // Opsional: hilangkan label debug
+      home: LoginPage(),
+    );
+  }
+}
